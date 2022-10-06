5544c6b9295c53aed8257b2bbd3a51af10f5a344
joni-deppu <44shuxichuan@gmail.com>
2022-10-06 18:31:00 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ User.create(name:"テスト",admin:"true",email:"test@00",password:"test00",encr
 TradingCompany.create(tc_name:"後で選択",tc_contact_person:"名無し",tc_telephone:"00000000000")
 Team.create(team_name:"い",affiliation:"いろは",work:"true")
 Schedule.create(scheduled_date:(Date.today)+1,time_zone_id:'2',accuracy_id:'2',size_id:'2',mie_id:'2',first_contact_id:'2',start_time_id:'2',
-                user_id:'1',team_id:'1',trading_company_id:'1')
+                prime_contractor:'テスト建設',content:'テスト',user_id:'1',team_id:'1',trading_company_id:'1')
 Schedule.create(scheduled_date:(Date.today)+2,time_zone_id:'3',accuracy_id:'3',size_id:'3',mie_id:'3',first_contact_id:'3',start_time_id:'7',
-                user_id:'1',team_id:'1',trading_company_id:'1')
+                prime_contractor:'テスト建設',user_id:'1',team_id:'1',trading_company_id:'1')
 Schedule.create(scheduled_date:(Date.today)+2,time_zone_id:'4',accuracy_id:'2',size_id:'2',mie_id:'4',first_contact_id:'3',start_time_id:'12',
-                user_id:'1',team_id:'1',trading_company_id:'1')
+                prime_contractor:'テスト建設',user_id:'1',team_id:'1',trading_company_id:'1')
 Schedule.create(scheduled_date:(Date.today)+3,time_zone_id:'2',accuracy_id:'4',size_id:'2',mie_id:'1',first_contact_id:'3',start_time_id:'12',
-                user_id:'1',team_id:'1',trading_company_id:'1')
+                prime_contractor:'テスト建設',user_id:'1',team_id:'1',trading_company_id:'1')
