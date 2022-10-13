a6c6c9068f905efdc66c4071f6d4fac5aae7a2f1
joni-deppu <44shuxichuan@gmail.com>
2022-10-13 18:31:46 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 # coding: utf-8
 User.create(name:"テスト",admin:"true",email:"test@00",password:"test00",encrypted_password:"$2a$12$8Nq8hYx0O009saTfH3kaA.eUFQmNjCHAO3nLvLMRuANurWgauflWW")
 TradingCompany.create(tc_name:"後で選択",tc_contact_person:"名無し",tc_telephone:"00000000000")
+TradingCompany.create(tc_name:"札幌商事",tc_contact_person:"函館",tc_telephone:"05000000000")
 Team.create(team_name:"い",affiliation:"いろは",work:"true")
 Schedule.create(scheduled_date:(Date.today)+1,time_zone_id:'2',accuracy_id:'2',size_id:'2',mie_id:'2',first_contact_id:'2',start_time_id:'2',prime_contractor:'テスト建設',content:'テスト',user_id:'1',team_id:'1',trading_company_id:'1')
 Schedule.create(scheduled_date:(Date.today)+2,time_zone_id:'3',accuracy_id:'3',size_id:'3',mie_id:'3',first_contact_id:'3',start_time_id:'7',prime_contractor:'テスト建設',content:'テスト',user_id:'1',team_id:'1',trading_company_id:'1')
